77232abdc64a64ddba7c3bd9259c7b9b51ce9c95
Muhammad Khalid <khalidanmol339@gmail.com>
2025-10-04 15:43:25 +0500

    setting the server and basic components

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+export default function Header() {
+  return (
+    <header className="app-header">
+      <svg>
+        <use href="../assets/react.svg"></use>
+      </svg>
+      <h1>The React Quiz App</h1>
+    </header>
+  );
+}
