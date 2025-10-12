504e2948a9a05d91e9050d3df0318407b057d578
Muhammad Khalid <khalidanmol339@gmail.com>
2025-10-12 12:26:45 +0500

    ""

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -7,7 +7,7 @@ export default function Timer() {
         }, 1000);
     }, [])
   return (
-    <div className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-2xl'>05:00</div>
+    <div className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-2xl'>05:00</div> 
   )
 }
  
